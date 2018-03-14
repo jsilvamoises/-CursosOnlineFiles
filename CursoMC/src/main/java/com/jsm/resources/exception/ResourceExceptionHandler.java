ab92d1d81d6561db0e787e0f2434eaa9980d2b78
developmentpdd <development.pdd@gmail.com>
2018-03-14 00:00:37 -0300

    Lista de cursos online

--- CursoMC/src/main/java/com/jsm/resources/exception/ResourceExceptionHandler.java
+++ CursoMC/src/main/java/com/jsm/resources/exception/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package com.jsm.resources.exception;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,6 +19,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<Erro> objectNotFoundExceptionHandler(ObjectNotFoundException ex, HttpServletRequest request){
 		Erro erro = new Erro(HttpStatus.NOT_FOUND.value(),ex.getMessage(),System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+		
+	}
+	
+	
+	@ExceptionHandler({ConstraintViolationException.class})
+	public ResponseEntity<Erro> constraintViolationExceptionHandler(ConstraintViolationException ex, HttpServletRequest request){
+		Erro erro = new Erro(HttpStatus.BAD_REQUEST.value(),ex.getCause()!=null?ex.getCause().getMessage():ex.getMessage(),System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
 		
 	}
 
